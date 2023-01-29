ad8caf4ccf43efe9c1dd5a2f6eb0154792c5a4d8
Hideo Oshima <orimajp@gmail.com>
2023-01-29 13:12:33 +0900

    エディタおよびプレビューコンポーネント初期追加

---
+++ document-editor/components/edit/EditMarkdownEditor.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { Monaco } from '@monaco-editor/loader'
+import type monaco from 'monaco-editor'
+import { debounce } from 'lodash'
+
+
+const editorElement = ref()
+
+</script>
+
+<template>
+  <div
+    ref="editorElement"
+    class="editor"
+  />
+</template>
+
+<style scoped>
+.editor {
+  text-align: left;
+}
+</style>
